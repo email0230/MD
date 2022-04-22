4aad3321062268211315e21df626b3210551b4db
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2022-04-22 16:52:35 +0200

    Added vertex drawing

--- MD_Projekt/MD_Projekt/MainWindow.xaml.cs
+++ MD_Projekt/MD_Projekt/MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace MD_Projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        Canvas canvas;
+        Vertex[] verts;
+        Ellipse ellipse;
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateCanvas();
+        }
+
+        private void CreateCanvas()
+        {
+            canvas = new Canvas();
+            canvas.Width = 600;
+            canvas.Height = 400;
+            canvas.Background = Brushes.Bisque;
+            canvas.Margin = new Thickness(175, 0, 0, 0);
+
+            grid.Children.Add(canvas);
+        }
+
+        private void Draw(object sender, RoutedEventArgs e)
+        {
+            CreateCanvas();
+
+            // getting input number
+            if (!int.TryParse(vertices.Text, out var numOfVertices))
+            {
+                return;
+            }
+
+            // creating vertex array
+            verts = new Vertex[numOfVertices];
+
+            for (int i = 0; i < numOfVertices; i++)
+            { 
+                verts[i] = new Vertex();
+            }
+
+            // drawing vertices
+            foreach (var vertex in verts)
+            {
+                ellipse = new Ellipse()
+                { 
+                    Width = 10,
+                    Height = 10
+                };
+
+                ellipse.Stroke = System.Windows.Media.Brushes.Black;
+                ellipse.Fill = System.Windows.Media.Brushes.DarkBlue;
+
+                ellipse.Margin = new Thickness(vertex.X, vertex.Y, 0, 0);
+                
+                canvas.Children.Add(ellipse);
+            }
+
+
+            /* example how to draw a line
+             * 
+            Line lll = new Line()
+            {
+                X1 = 50,
+                Y1 = 50,
+                X2 = 190,
+                Y2 = 100,
+                Stroke = Brushes.Blue,
+                StrokeThickness = 2.5
+            };
+            grid.Children.Add(lll);
+            */
         }
     }
 }
---
+++ MD_Projekt/MD_Projekt/Vertex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD_Projekt
+{
+    internal class Vertex
+    {
+
+        public int X;
+        public int Y;
+
+        public Vertex()
+        {
+            Random random = new Random();
+
+            // 25px margin
+            X = 25 + random.Next(550); 
+            Y = 25 + random.Next(350);
+        }
+    }
+}
